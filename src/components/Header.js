ec44a2ec5e4cc1e1ac161d1d4bba3311c8747431
Arif Bashar <arifbasharx@gmail.com>
2020-08-07 18:00:25 -0500

    added onclick method

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Hamburger from "./Hamburger"
+import Hamburger from "./Hamburger";
 
 const Header = () => {
-  return <header>
-    <div className="container">
-      <div className="wrapper">
-        <div className="inner-header">
-          <div className="logo">
-            <Link to="/">HAMBRG.</Link>
-          </div>
-          <div className="menu">
-            <button>
-              Menu
-            </button>
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+  });
+
+  const handleMenu = () => {
+    if (state.initial === false) {
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close",
+      });
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+    }
+  };
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">HAMBRG.</Link>
+            </div>
+            <div className="menu">
+              <button onClick={handleMenu}>Menu</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <Hamburger />
-  </header>;
+      <Hamburger />
+    </header>
+  );
 };
 
 export default Header;
