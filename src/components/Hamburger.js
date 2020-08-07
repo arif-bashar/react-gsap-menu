4a79b041566b77fecd15a6b67f792aeeb45344e8
Arif Bashar <arifbasharx@gmail.com>
2020-08-07 17:29:32 -0500

    implemented react-router w/ working links and styles on main homepage container

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hamburger = () => {
-  return <div className=''>menu</div>;
+  return (
+    <div className="hamburger-menu">
+      <div className="menu-secondary-background-color"></div>
+      <div className="menu-layer">
+        <div className="menu-city-background"></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/opportunities">Opportunities</Link>
+                  </li>
+                  <li>
+                    <Link to="/solutions">Solutions</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact-us">Contact Us</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className="info">
+                <h3>Our Promise</h3>
+                <p>
+                  The purpose of lorem ipsum is to create a natural looking
+                  block of text (sentence, paragraph, page, etc.) that doesn't
+                  distract from the layout. A practice not without controversy,
+                  laying out pages with meaningless filler text can be very
+                  useful when the focus is meant to be on design, not content.
+                </p>
+              </div>
+              <div className="locations">
+                Locations: 
+                <span>Dallas</span>
+                <span>Austin</span>
+                <span>New York</span>
+                <span>San Francisco</span>
+                <span>Beijing</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hamburger;
